66e47b45cd870974a6f868bb0f28400ca9ad0b5f
beruma90 <bruiz@alutelmobility.com>
2023-11-03 10:54:10 -0600

    Initial commit

---
+++ MBC_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MBC_Test
+//
+//  Created by Berny Ruiz on 3/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
